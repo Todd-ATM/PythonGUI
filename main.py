5e97755e081e5cd7d9cbbdec4312c1b21f2c6cc7
Todd-ATM <ToddJr.Acrossthemountain@gmail.com>
2025-06-05 15:46:37 -0700

    added function to change our label text, connected signal from QPushbutton to call changeText function when clicked

--- main.py
+++ main.py
@@ -13,12 +13,23 @@ window.resize(300, 200)
 #create label and pushbutton
 label = QLabel(window)
 label.setText("Hello")
-button = QPushButton("text", window)
+button = QPushButton("text")
 
 #create vertical layout, add our label and button to it
 layout = QVBoxLayout(window)
 layout.addWidget(label)
 layout.addWidget(button)
+#setup layout
+layout.setSpacing(100)
+
+#function that sets the label to a different text
+def changeText():
+    label.setText("Changed the Text of the label")
+
+#setup our buttons connect signal to call changeText() function
+button.clicked.connect(changeText)
+
+
 window.show()
 
 app.exec()
