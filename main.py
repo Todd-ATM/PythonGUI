ed3f039f09b17ef44b20b0150420e281f025220e
Todd-ATM <ToddJr.Acrossthemountain@gmail.com>
2025-06-05 15:19:33 -0700

    created push button and added to vertical layout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sys
-from PySide6.QtWidgets import  QApplication, QWidget, QLabel, QInputDialog, QVBoxLayout
+from PySide6.QtWidgets import  QApplication, QWidget, QLabel, QInputDialog, QVBoxLayout, QPushButton
 
 
 #set main Application using Pythons Sys arguments
@@ -9,12 +9,16 @@ app = QApplication(sys.argv)
 window = QWidget()
 window.setWindowTitle("My First GUI App")
 window.resize(300, 200)
+
+#create label and pushbutton
 label = QLabel(window)
 label.setText("Hello")
-label.alignment()
-#create vertical layout, add our label to it
+button = QPushButton("text", window)
+
+#create vertical layout, add our label and button to it
 layout = QVBoxLayout(window)
 layout.addWidget(label)
+layout.addWidget(button)
 window.show()
 
 app.exec()
